cbda28b95cc6e62249e4e207d965ba1a8aec90de
MacMini <>
2023-05-21 23:51:09 +0600

    App created

---
+++ PocketPal/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PocketPal
+//
+//  Created by itiw-mac 256 on 5/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PocketPal/PocketPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketPalApp.swift
+//  PocketPal
+//
+//  Created by itiw-mac 256 on 5/21/23.
+//
+
+import SwiftUI
+
+@main
+struct PocketPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
